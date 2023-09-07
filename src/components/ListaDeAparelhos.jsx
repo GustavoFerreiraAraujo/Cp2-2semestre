71bdd1e760a13561a4d81a3a4b284f097b615430
rm550181 <rm550181@fiap.com.br>
2023-09-06 21:07:16 -0300

    ---

--- src/components/ListaDeAparelhos.jsx
+++ src/components/ListaDeAparelhos.jsx
@@ -1,10 +1,16 @@
+import Iphone13 from "/src/assets/img/Iphone13.jpg";
+import Mi11Ultra from "/src/assets/img/mi11ultra.png";
+import GalaxyS21Ultra from "/src/assets/img/galaxys21ultra.png";
+import IpadPro from "/src/assets/img/ipadpro.png";
+import GalaxyTabS7 from "/src/assets/img/galaxytabs7.jpg";
+
 export const ListaDeAparelhos = [
     {
         id:1,
         nome: "Iphone 13",
         marca: "Apple",
         preco: 5000,
-        imagem: "/src/assets/img/Iphone13.jpg",
+        imagem: {Iphone13},
         descricao: "O iPhone 13 é um smartphone da Apple que foi lançado em 2021. Ele tem um design bonito e está disponível em várias cores. A tela é muito boa, com cores vivas e brilho alto. O iPhone 13 é rápido graças ao seu chip A15 Bionic, o que significa que os aplicativos funcionam sem problemas. A câmera tira fotos nítidas e tem um modo noturno para fotos em ambientes com pouca luz. Você pode gravar vídeos em alta qualidade. O telefone também é compatível com redes 5G para uma conexão rápida à internet. A bateria dura bastante, então você não precisa recarregar o telefone o tempo todo. No geral, o iPhone 13 é uma ótima opção para quem quer um telefone confiável e com boas câmeras."
     },
 
@@ -13,7 +19,7 @@ export const ListaDeAparelhos = [
         nome: "Mi 11 Ultra ",
         marca: "Xiaomi",
         preco: 3000,
-        imagem: "/src/assets/img/mi11ultra.png",
+        imagem: {Mi11Ultra},
         descricao:"O Xiaomi Mi 11 Ultra é um smartphone muito poderoso da Xiaomi. Ele tem uma tela grande e bonita com cores vívidas. O telefone é rápido e pode lidar facilmente com jogos e aplicativos exigentes. A melhor parte é a câmera, que tira fotos incríveis com muitos detalhes, especialmente em ambientes escuros. Além disso, a bateria dura bastante tempo, e você pode carregá-la rapidamente. Este é um ótimo telefone para quem procura desempenho e uma excelente qualidade de câmera."
     }, 
 
@@ -22,7 +28,7 @@ export const ListaDeAparelhos = [
         nome: " Galaxy S21 Ultra",
         marca: "Samsung",
         preco: 4000,
-        imagem: "/src/assets/img/galaxys21ultra.png",
+        imagem: {GalaxyS21Ultra},
         descricao:"O Samsung Galaxy S21 Ultra é um dos smartphones mais potentes da Samsung. Ele tem uma tela grande e impressionante, que exibe imagens muito nítidas e coloridas. O telefone é super rápido e pode executar facilmente qualquer aplicativo ou jogo. A câmera é excelente, com muitos megapixels, ótimo para tirar fotos de alta qualidade, especialmente com zoom. A bateria é grande e dura bastante tempo, e você pode carregá-la rapidamente. Este é um ótimo telefone para quem busca desempenho excepcional e uma qualidade de câmera superior."
     },
 
@@ -31,7 +37,7 @@ export const ListaDeAparelhos = [
         nome : "iPad Pro 2021",
         marca: "Apple",
         preco: 6000,
-        imagem: "/src/assets/img/ipadpro.png",
+        imagem: {IpadPro},
         descricao:"A tela é linda e brilhante, ótima para assistir a filmes ou trabalhar em projetos criativos. Ele também é compatível com a Apple Pencil e o Magic Keyboard, tornando-o ideal para desenhar e digitar. A câmera tira ótimas fotos e vídeos, e há opções de armazenamento generosas para guardar todos os seus arquivos."
     },
 
@@ -40,7 +46,7 @@ export const ListaDeAparelhos = [
         nome: "Samsung Galaxy Tab S7",
         marca: "Samsung",
         preco: 4000,
-        imagem: "/src/assets/img/galaxytabs7.jpg",
+        imagem: {GalaxyTabS7},
         descricao:"O Samsung Galaxy Tab S7+ é um tablet poderoso feito pela Samsung. Ele tem uma tela grande e bonita que mostra cores vivas. O tablet é rápido e pode ser usado para muitas coisas, como navegar na internet, jogar jogos e trabalhar. Você pode até mesmo usar uma caneta especial chamada S Pen para desenhar ou escrever na tela. O Galaxy Tab S7+ vem com várias opções de armazenamento para guardar aplicativos e arquivos. Alguns modelos também têm conectividade 5G para internet rápida. É uma ótima escolha para quem busca um tablet Android de alto desempenho."
     }
 ]
